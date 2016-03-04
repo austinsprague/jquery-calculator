f9b30377adc8d1fc79ed7190df9742bfd33d46f9
austinsprague <sprague.ak@gmail.com>
2016-03-03 21:47:32 -0800

    Operators working and equals

--- js/main.js
+++ js/main.js
@@ -7,45 +7,45 @@ $(document).ready(function($) {
   var right = 0;
   var result = 0;
 
-    $('span').not('.operator').click(function(event){
+    $('span').not('.operator, #calc').click(function(event){
       var $this = $(this);
       var content = $this.text();
-      // number = parseInt(content);
-      // console.log('this is a number');
       if (operator == null){
         left = left + content;
-        // result = result + content;
         $screen.text(left);
       }
       else if (operator != null) {
         right = right + content;
         $screen.text(left + operator + right);
       }
-      // result = left + operator + right;
-      // $screen.text(result);
     });
 
-    $('.operator').click(function(event){
+    $('.operator').not('#calc').click(function(event){
       var $this = $(this);
       var content = $this.text();
-      // console.log('this is an operator');
       operator = content;
-      // console.log(content);
       $screen.text(left + content);
     });
     $('#calc').click(function(event){
-      // console.log('Equals was clicked')
       var leftNum = parseInt(left);
       var rightNum = parseInt(right);
       if(operator === '+'){
         result = leftNum + rightNum;
+        console.log(result);
         $screen.text(result);
       }
       else if (operator === '-') {
         result = leftNum - rightNum;
         $screen.text(result);
       }
-
+      else if (operator === 'x') {
+        result = leftNum * rightNum;
+        $screen.text(result);
+      }
+      else {
+        result = leftNum / rightNum;
+        $screen.text(result);
+      }
     });
 
 
