5ff4fe5cb8542b1643167650be80f07ee506479a
austinsprague <sprague.ak@gmail.com>
2016-03-03 20:51:13 -0800

    Screen showing numbers and operators

--- js/main.js
+++ js/main.js
@@ -3,89 +3,53 @@ $(document).ready(function($) {
 
   var $screen = $('#screen');
   var operator;
-  var left;
-  var right;
-  var result;
+  var left = 0;
+  var right = 0;
+  var result = 0;
 
     $('span').not('.operator').click(function(event){
       var $this = $(this);
       var content = $this.text();
-      number = parseInt(content);
-      console.log('this is a number');
-
-      $screen.text(number);
+      // number = parseInt(content);
+      // console.log('this is a number');
+      if (operator == null){
+        left = left + content;
+        // result = result + content;
+        $screen.text(left);
+      }
+      else if (operator != null) {
+        right = right + content;
+        $screen.text(left + operator + right);
+      }
+      // result = left + operator + right;
+      // $screen.text(result);
     });
 
     $('.operator').click(function(event){
-      console.log('this is an operator')
-      if('.operator === +;'){
-        result = left + right;
+      var $this = $(this);
+      var content = $this.text();
+      // console.log('this is an operator');
+      operator = content;
+      // console.log(content);
+      $screen.text(left + content);
+    });
+    $('#calc').click(function(event){
+      // console.log('Equals was clicked')
+      var leftNum = parseInt(left);
+      var rightNum = parseInt(right);
+      if(operator === '+'){
+        result = leftNum + rightNum;
+        $screen.text(result);
+      }
+      else if (operator === '-') {
+        result = leftNum - rightNum;
+        $screen.text(result);
       }
 
-
-
-    // })
-    // $('#calc').click(function(event){
-    //   var $this = $(this);
-    //   if ("$('.operator') === '+')"){
-    //     result = 1+2;
-    //     console.log(result);
-    //   }
-    //   else if ("$('.operator') === '-')") {
-    //     result = 1-2;
-    //     console.log(result);
-    //   }
-    //
-    // })
-
-
-  // $("span.operator:contains('+')").click(function(event){
-  //   var $this = $(this);
-  //   var content = $this.text();
-  //   total += number;
-  //   $screen.text(content);
-  //   console.log('plus')
-  //   console.log('thie totla' + total)
-  //   }
-  // )
-  // $("span.operator:contains('-')").click(function(event){
-  //   var $this = $(this);
-  //   var content = $this.text();
-  //   // total += number;
-  //   $screen.text(content);
-  //   }
-  // )
-  // $("span.operator:contains('=')").click(function(event){
-  //   console.log('the total is ' + total);
-  //
-  //   $screen.text(total);
-  //   }
-  // )
+    });
 
 
 
 
 });
 })();
-
-
-//____Clear #screen of count
-//____Total #screen with = // $screen.text(total);
-//____Multiply #screen
-//____Divide #screen contents
-//____Add contents
-
-
-
-
-// for (var i=0; i<9; i++){
-//
-//
-//   if ($("span:contains('[i]')")) {
-//   console.log('one clicked');
-//
-// }
-
-// if ($("span:contains('1')")) {
-//   console.log('one clicked');
-// }
